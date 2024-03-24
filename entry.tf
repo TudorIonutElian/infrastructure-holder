caf01699e84c44817883a0bd5bec1044c497f66d
Ionut Elian TUDOR <ionuteliantudor@gmail.com>
2024-03-24 22:48:28 +0200

    Add some docs

--- entry.tf
+++ entry.tf
@@ -1,10 +1,22 @@
-resource "null_resource" "run_shell_script" {
-  # You can specify triggers here if necessary.
 
+/**
+ * Resource: null_resource.run_shell_script
+ * 
+ * Description:
+ * This resource represents a null resource that can be used to run shell scripts.
+ * 
+ * Usage:
+ * This resource can be used to execute shell scripts as part of the Terraform provisioning process.
+ * 
+ * Example:
+ * resource "null_resource" "run_shell_script" {
+ *   # Configuration options for the null_resource
+ * }
+ */
+resource "null_resource" "run_shell_script" {
   provisioner "local-exec" {
     command = "bash ./scripts/generate-variables.sh"
   }
 
-  # Ensure this resource runs first
   depends_on = [null_resource.run_shell_script]
 }
--- helpers/clear-variables.sh
+++ helpers/clear-variables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Function to clear the contents of the variables.tf file.
+# This function empties the variables.tf file by overwriting it with an empty file.
 function clearVariables() {
     > variables.tf
 }
